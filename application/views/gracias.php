a1c0bdadc36cfe2dd05132f2d1964d096031dabc
ximenapaparella <ximenapaparella@hotmail.com>
2015-02-10 10:50:03 -0300

    Update gracias.php

--- application/views/gracias.php
+++ application/views/gracias.php
@@ -68,7 +68,7 @@
 
 
             <h2 style="text-align:center;">Recomendá a tus amigos lo fácil que es regalar en Spa Belgrano!</h2>
-            <div style="text-align:center"><!-- Go to www.addthis.com/dashboard to customize your tools -->
+            <div style="text-align:center; height:200px;"><!-- Go to www.addthis.com/dashboard to customize your tools -->
 	    <div style="text-align:center" class="addthis_native_toolbox"></div>
 	    <div>	
 
