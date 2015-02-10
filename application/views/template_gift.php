213fb573a545c9a1b57d90f9693bd016b05125f0
ximenapaparella <ximenapaparella@hotmail.com>
2015-02-10 12:22:01 -0300

    Cambio de plantilla

--- application/views/template_gift.php
+++ application/views/template_gift.php
@@ -2,378 +2,54 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
-<title>Spa Belgrano</title>
-<style type="text/css">
-.ReadMsgBody {
-	width: 100%;
-	background-color: #ffffff;
-}
-.ExternalClass {
-	width: 100%;
-	background-color: #ffffff;
-}
-.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
-	line-height: 100%;
-}
-html {
-	width: 100%;
-}
-body {
-	-webkit-text-size-adjust: none;
-	-ms-text-size-adjust: none;
-}
-body {
-	margin: 0;
-	padding: 0;
-}
-table {
-	border-spacing: 0;
-}
-img {
-	display: block !important;
-}
-table td {
-	border-collapse: collapse;
-}
-.yshortcuts a {
-	border-bottom: none !important;
-}
-a {
-	color: #b54e65;
-	text-decoration: none;
-}
- @media only screen and (max-width: 640px) {
-body {
-	width: auto !important;
-}
-table[class="table600"] {
-	width: 450px !important;
-	text-align: center !important;
-}
-table[class="table-inner"] {
-	width: 86% !important;
-	text-align: center !important;
-}
-table[class="table2-2"] {
-	width: 47% !important;
-}
-table[class="table3-3"] {
-	width: 100% !important;
-	text-align: center !important;
-}
-table[class="table1-3"] {
-	width: 29.9% !important;
-}
-table[class="table3-1"] {
-	width: 64% !important;
-	text-align: center !important;
-}
-table[class="footer-note"] {
-	width: 100% !important;
-	text-align: left !important;
-}
-table[class="gallery"] {
-	width: 29.4% !important;
-	text-align: center !important;
-}
-/* Image */
-img[class="img1"] {
-	width: 100% !important;
-	height: auto !important;
-}
-img[class="button"] {
-	width: 100% !important;
-	height: auto !important;
-}
-/*Footer*/
-table[class="footer-column"] {
-	width: 47% !important;
-	text-align: left !important;
-}
-}
- @media only screen and (max-width: 479px) {
-body {
-	width: auto !important;
-}
-table[class="table600"] {
-	width: 290px !important;
-}
-table[class="table-inner"] {
-	width: 80% !important;
-}
-table[class="table2-2"] {
-	width: 100% !important;
-}
-table[class="table3-3"] {
-	width: 100% !important;
-	text-align: center !important;
-}
-table[class="table1-3"] {
-	width: 100% !important;
-}
-table[class="table3-1"] {
-	width: 100% !important;
-	text-align: center !important;
-}
-table[class="middle-line"] {
-	display: none !important;
-}
-table[class="footer-note"] {
-	width: 100% !important;
-	text-align: center !important;
-}
-table[class="gallery"] {
-	width: 100% !important;
-	text-align: center !important;
-}
-/* image */
-img[class="img1"] {
-	width: 100% !important;
-}
-img[class="button"] {
-	width: 70% !important;
-	height: auto !important;
-}
-/*Footer*/
-table[class="footer-column"] {
-	width: 100% !important;
-	text-align: center !important;
-}
-}
-</style>
+<title>Documento sin título</title>
 </head>
 
 <body>
-	<table width="100%" border="0" cellspacing="0" cellpadding="0" style="background-color:#FFF">
-		<tr>
-			<td valign="top">
-				<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-					<!--Header Bar-->
-					<tr>
-						<td>
-							<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-								<tr>
-									<td height="35" align="center" valign="top" style="border-top:4px solid #bdc3c7; background-color:#fffff;">
-										<img src="http://www.spabelgrano.com/newsletter/news-base/news-1/img/header-shape.png" width="24" height="10" alt="header-shape" />
-									</td>
-								</tr>
-							<tr>
-							<td valign="bottom" style="background-color:#FFFFFF;" >
-								<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-									<tr>
-										<td style="background-color:#FFF;">
-											<table style="background-color:#949699" class="table600" width="610" border="0" align="center" cellpadding="0" cellspacing="0">
-												<tr>
-													<td height="80" align="center">
-														<table class="table-inner" width="565" border="0" align="center" cellpadding="0" cellspacing="0">
-															<tr>
-																<td colspan="2"></td>
-															</tr>
-															<tr>
-																<td width="450" height="30">
-																	<a href="http://www.spabelgrano.com">
-																		<img src="http://www.spabelgrano.com/newsletter/news-base/news-1/img/logo.png" width="187" height="49" border="0" />
-																	</a>
-																</td>
-																<td width="115">
-																	<table width="100%" border="0">
-																		<tr>
-																			<td>
-																				<a href="https://www.facebook.com/spabelgrano?fref=ts">
-																					<img src="http://www.spabelgrano.com/newsletter/news-base/news-1/img/facebook.png" width="27" height="33" border="0" />
-																				</a>
-																			</td>
-																			<td>
-																				<a href="https://twitter.com/SpaBelgrano">
-																					<img src="http://www.spabelgrano.com/newsletter/news-base/news-1/img/twitter.png" width="27" height="33" border="0" />
-																				</a>
-																			</td>
-																			<td>
-																				<a href="mailto:info@spabelgrano.com">
-																					<img src="http://www.spabelgrano.com/newsletter/news-base/news-1/img/globo.png" width="27" height="33" border="0" />
-																				</a>
-																			</td>
-																		</tr>
-																	</table>
-																</td>
-															</tr>
-														</table>
-													</td>
-												</tr>
-											</table>
-										</td>
-									</tr>
-									<tr>
-										<td bgcolor="#ffffff">
-											<table width="600" border="0" align="center" cellpadding="0" cellspacing="0" class="table600">
-												<tr>
-													<td>
-														<img class="img1" src="http://www.spabelgrano.com/newsletter/news-base/news-1/img/shadow.png" width="600" height="8" alt="shadow" />
-													</td>
-												</tr>
-											</table>
-										</td>
-									</tr>
-								</table>
-							</td>
-						</tr>
-					</table>
-				</td>
-			</tr>
-			<!--End Header Bar-->
-			<!-- Header image --> <!--end header image-->
-			<!-- Header text -->
-			<tr>
-				<td>
-					<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-						<tr>
-							<td style="background-color:#FFFFFF;">
-								<table bgcolor="#949699" class="table600" width="610" border="0" align="center" cellpadding="0" cellspacing="0">
-									<tr>
-										<td align="center">
-											<table class="table-inner" width="565" border="0" align="center" cellpadding="0" cellspacing="0">
-												<tr>
-													<td height="10">
-
-													</td>
-												</tr>
-												<tr align="left" valign="top">
-													<td style="font-family: 'Open Sans', Arial, sans-serif; color:#ffffff; font-size:18px; font-weight:100; text-align:center;">REENVIA ESTE CORREO A LA PERSONA QUE LE QUERES REGALAR EL GIFT Y QUE EMPIECE A DISFRUTAR!
-													</td>
-												</tr>
-												<tr>
-													<td height="15">
-													</td>
-												</tr>
-												<tr>
-													<td height="10">
-													</td>
-												</tr>
-											</table>
-										</td>
-									</tr>
-								</table>
-							</td>
-						</tr>
-						<tr>
-							<td bgcolor="#ffffff">
-								<table width="600" border="0" align="center" cellpadding="0" cellspacing="0" class="table600">
-									<tr>
-										<td>
-											<img class="img1" src="http://www.spabelgrano.com/newsletter/news-base/news-1/img/shadow.png" width="600" height="18" alt="shadow" />
-										</td>
-									</tr>
-								</table>
-							</td>
-						</tr>
-					</table>
-				</td>
-			</tr>
-								<!--end header text-->
-
-								<!--1/1 content--> <!-- end 1/1 content -->
-
-								<!-- divider -->
-				<tr>
-					<td><table bgcolor="#ffffff" width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-						<tr>
-							<td align="center">
-								<img src="http://www.spabelgrano.com/newsletter/news-base/news-1/img/divider.png" width="182" height="7" alt="divider" />
-								<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-									<tr>
-										<td style="background-color:#FFFFFF;">
-											<table class="table600" width="600" border="0" align="center" cellpadding="0" cellspacing="0">
-												<tr>
-													<td height="330" style="background-image:url(<?php echo base_url('/assets/template/images/mailing/'); ?>/gift.png); background-repeat:no-repeat; background-position:center;" align="center" ><table   width="90%" border="0">
-														<tr>
-															<td style="padding-left:95px;padding-right:65px;">
-																<p style="font-size:13px; font-family:Arial, Helvetica, sans-serif; color:#777; line-height:15px; padding-top:35px; text-align:left;">Patricio,
-																</p>
-															</td>
-														</tr>
-														<tr>
-															<td height="67"  style="padding-left:95px;padding-right:65px;">
-																<p style="font-size:13px; font-family:Arial, Helvetica, sans-serif; color:#777; line-height:15px; text-align:left;">Mensaje de hasta 150 caracteres.
-																</p>
-															</td>
-														</tr>
-														<tr>
-															<td style="padding-left:95px;padding-right:65px;">
-																<p style="font-size:13px; font-family:Arial, Helvetica, sans-serif; color:#777; line-height:15px; text-align:right;">Ximena
-																</p>
-															</td>
-														</tr>
-														<tr>
-															<td height="44">
-																<p style="font-size:13px; font-family:Arial, Helvetica, sans-serif; color:#777; line-height:15px; text-align:center;" >Tratamiento Seleccionado
-																</p>
-															</td>
-														</tr>
-														<tr>
-															<td height="59">
-																<p style="font-size:10px; font-family:Arial, Helvetica, sans-serif; color:#666; text-align:center;">Válido hasta el 23-may-2015 | Código: A5426
-																</p>
-															</td>
-														</tr>
-													</table>
-												</td>
-											</tr>
-										</table>
-									</td>
-								</tr>
-							</table>
-						</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-		<!-- end divider -->
-
-		<!--1/2 panel [right text]--> <!-- end 1/2 panel [right text] -->
-
-		<!--1/2 panel [left text]--> <!-- end 1/2 panel [left text] -->
-
-		<!-- quote --> <!-- end quote -->
-
-		<!-- content with 2 buttons --> <!-- end content with 2 buttons -->
-
-		<!--2/2 panel--> <!--end 2/2 panel-->
-
-		<!--3/3 gallery-->
-		<!--end 3/3 gallery-->
-
-		<!--1/3 panel [right text]--> <!--end 1/3 panel [right text]-->
-
-		<!--1/3 panel [left text]--> <!--end 1/3 panel [left text]-->
-		<!-- 3/3 panel --> <!-- end 3/3 panel -->
-
-		<!--2/2 content-->
-		<!--end 2/2 content-->
-
-		<!--table-->
-		<!--end table-->
-		<!-- footer -->
-		<tr>
-			<td  style="border-top:3px solid #bec0c1; border-bottom:2px solid #bec0c1;" bgcolor="#58595b" height="30">
-				<table class="table600" width="600" border="0" align="center" cellpadding="0" cellspacing="0">
-					<tr>
-						<td align="center" style="font-family: 'open sans',Arial, sans-serif; font-size:13px; color:#ffffff; line-height:26px;">
-							Virrey del Pino 2237 Belgrano - 4784.3333 -
-							<a style="color:#fff;" href="mailto:info@spabelgrano.com">
-								info@spabelgrano.com
-							</a>
-							-
-							<a style="color:#fff;" href="http://www.spabelgrano.com">spabelgrano.com</a>
-						</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-		<!-- end footer -->
-	</table>
-</td>
-</tr>
+<table width="661" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td height="229" colspan="3"><?php echo base_url('/assets/template/images/mailing/'); ?>/top.png);</td>
+  </tr>
+  <tr>
+    <td width="103" height="286" rowspan="2"><?php echo base_url('/assets/template/images/mailing/'); ?>/left.png);</td>
+    <td width="432" style="background-color:#f0f0ef" height="247">
+    <table cellpadding="0" style="background-color:#f0f0ef;" cellspacing="0" height="247" width="432" border="0">
+      <tr>
+        <td height="30"><p style="font-family:Arial, Helvetica, sans-serif; margin: 0px; font-size:13px; color:#8d8d8c;">Patricio,</p></td>
+      </tr>
+      <tr>
+        <td height="80"><p style="font-family:Arial, Helvetica, sans-serif; margin: 0px; font-size:13px; color:#8d8d8c;">Esto es una prueba de un mensaje de 150 caracteres para ser utilizado como base para enviar mediante mail en el sistema de gift certificate de spa bel</p></td>
+      </tr>
+      <tr>
+        <td height="30"><p style="font-family:Arial, Helvetica, sans-serif; margin: 0px; font-size:13px; text-align:right; color:#8d8d8c;">Ximena</p></td>
+      </tr>
+      <tr>
+        <td height="30"><p style="font-family:Arial, Helvetica, sans-serif; margin: 0px; font-size:13px; text-align:center; color:#8d8d8c;">Gift Certificate Válido por: Masajes Relajantes y Descontracturantes</p></td>
+      </tr>
+      <tr>
+        <td height="30" align="center" ><?php echo base_url('/assets/template/images/mailing/'); ?>/stars.png);</tr>
+      <tr>
+        <td height="30" align="center"><p style="font-family:Arial, Helvetica, sans-serif; font-size:11px; margin: 0px; color:#8d8d8c;">Válido hasta el 05-10-2015 | Código: AGR45G</p></td>
+      </tr>
+    </table></td>
+    <td width="126" height="286" rowspan="2"><?php echo base_url('/assets/template/images/mailing/'); ?>/right.png);</td>
+  </tr>
+  <tr>
+    <td height="39"><?php echo base_url('/assets/template/images/mailing/'); ?>/bottom-prev.png);</td>
+  </tr>
+  <tr>
+    <td colspan="3"><?php echo base_url('/assets/template/images/mailing/'); ?>/bottom.png);</td>
+  </tr>
 </table>
+
+<map name="Map" id="Map">
+  <area shape="rect" coords="197,60,325,86" href="https://twitter.com/SpaBelgrano" />
+  <area shape="rect" coords="365,63,483,87" href="https://www.facebook.com/spabelgrano?fref=ts" />
+  <area shape="rect" coords="209,29,360,52" href="mailto:info@spabelgrano.com" />
+</map>
+
+<map name="Map2" id="Map2">
+  <area shape="rect" coords="14,12,288,90" href="http://www.spabelgrano.com" />
+</map>
 </body>
 </html>
