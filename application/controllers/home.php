2aa97cb1a791d283ae62117bd1bb63e9185a19de
ximenapaparella <ximenapaparella@hotmail.com>
2015-02-10 15:47:34 -0300

    Update home.php

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -97,8 +97,7 @@ class Home extends CI_Controller {
 
 
 			$gift = $this->gift_model->get_all_gifts($id_venta);
-			var_dump ($gift);
-			die;
+			
 			foreach ($gift AS  $gif)
 			{
 				$message = $this->load->view('template_gift',$gif,TRUE);
